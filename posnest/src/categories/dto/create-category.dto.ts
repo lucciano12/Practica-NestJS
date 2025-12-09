e95f733d500c4ffa3f4e68caf17880ab317d0431
Lucciano99 <lpradoe@hotmail.com>
2025-12-09 12:20:44 -0300

    Añadir módulo de categorías con integración con TypeORM
    -Se introdujo un nuevo módulo de categorías con controlador, servicio, DTO y entidad para la gestión de categorías.
    -Se integró TypeORM con PostgreSQL mediante un nuevo archivo de configuración.
    -Se actualizó main.ts para que sea compatible con el nuevo módulo y se añadieron las dependencias necesarias para TypeORM, la validación y PostgreSQL.

---
+++ posnest/src/categories/dto/create-category.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString({ message: 'El nombre de la categoria no puede ir vacio' })
+  @ApiProperty({ example: 'Bebidas', description: 'Nombre de la categoria' })
+  name: string;
+
+  @IsNotEmpty({ message: 'La descripcion de la categoria no puede ir vacia' })
+  @ApiProperty({ example: 'Categoria para bebidas frias y calientes', description: 'Descripcion de la categoria'})
+  description: string;
+}
--- posnest/src/main.ts
+++ posnest/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  ); //Habilitamos los pipes globales, para validacion de DTOs
 
   const config = new DocumentBuilder() //Inicializamos swagger ui
     .setTitle('PosNest API')
